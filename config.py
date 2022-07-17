265acd781d5d34884a0cbf36bd4698a7575cd75e
熊崎章吾 <white.march322@gmail.com>
2022-07-17 17:44:14 +0900

    Add: インサートのフラグ作成

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from typing import Tuple, List, Set
 
-grade_race = 'トヨタ賞中京記念'
-# grade_race = None # 重賞分析の時はコメントアウトする
+# grade_race = 'トヨタ賞中京記念'
+grade_race = None # 重賞分析の時はコメントアウトする
 
 """
 開催場所
@@ -16,11 +16,11 @@ is_turf: bool = True  # 芝
 curse = '芝' if is_turf else 'ダート'
 
 ### 距離
-length: int = 1200
+length: int = 2000
 
 ### 馬場コンディション 使う方をコメントイン
-# cond: str = '良'
-cond: str = '重'
+cond: str = '良'
+# cond: str = '重'
 
 ### 開催日のいつか
 days_pattern: dict = {
@@ -34,6 +34,9 @@ days_pattern: dict = {
 display_day = {1:'開幕週', 2:'2週目だけ', 3:'3週目だけ', 4: '最終週', 5:'開催前半', 6:'開催後半'}
 select_day = 4
 days: Tuple = days_pattern[select_day]
+
+is_insert = True
+is_insert = False ## 分析データをインサートしたい時はコメントアウト
 
 if grade_race is not None:
   print(f'重賞分析:{grade_race}')
